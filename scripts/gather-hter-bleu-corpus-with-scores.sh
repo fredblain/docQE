59c81b0ba5c0937d4fe6247b7238ffffb9418bc4
Fred Blain <f.blain@sheffield.ac.uk>
2019-05-29 15:16:33 +0100

    adding year range to filter data from.

--- scripts/gather-hter-bleu-corpus-with-scores.sh
+++ scripts/gather-hter-bleu-corpus-with-scores.sh
@@ -16,13 +16,20 @@
 # in order to train a word- and sentence-level QE model
 ###
 
-pushd ../filelists-and-doc-scores
+## '-filtered' means top submission + one bottom one only, per task, per year
+## '' all submissions otherwise
+filt='-filtered'
+# filt=''
 
 srclang=english
 src=en
 
-# filt=''
-filt='-filtered'
+## starting/ending year dfining the range of edition to consider
+## to create the dataset
+startingyear=2011
+endingyear=2017
+
+pushd ../filelists-and-doc-scores
 
 for target in 'german-de' 'russian-ru'; do
 # for target in 'russian-ru'; do
@@ -32,22 +39,22 @@ for target in 'german-de' 'russian-ru'; do
 
   echo "processing systems for ${tgtlang}${filt}..."
   
-  rm -f /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}
-  touch /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}
+  rm -f /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}
+  touch /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}
   
-  rm -f /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt}
-  touch /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt}
+  rm -f /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}
+  touch /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}
   
-  rm -f /tmp/${srclang}-${tgtlang}.ref${filt}.${src}
-  touch /tmp/${srclang}-${tgtlang}.ref${filt}.${src}
+  rm -f /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${src}
+  touch /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${src}
 
-  rm -f /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.bleu
-  touch /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.bleu
+  rm -f /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.bleu
+  touch /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.bleu
   
-  for year in {2011..2018}; do
+  for year in $(seq ${startingyear} ${endingyear}); do
     echo "processing ${year}..."
-    # cat ${year}.${srclang}-${tgtlang}.filelist-systems${filt}.${tgt} > /tmp/${year}.${srclang}-${tgtlang}.systems${filt}.${tgt}
-    # cat ${year}.${srclang}-${tgtlang}.filelist-alignref${filt}.${tgt} > /tmp/${year}.${srclang}-${tgtlang}.ref${filt}.${tgt}
+    ## cat ${year}.${srclang}-${tgtlang}.filelist-systems${filt}.${tgt} > /tmp/${year}.${srclang}-${tgtlang}.systems${filt}.${tgt}
+    ## cat ${year}.${srclang}-${tgtlang}.filelist-alignref${filt}.${tgt} > /tmp/${year}.${srclang}-${tgtlang}.ref${filt}.${tgt}
     paste ${year}.${srclang}-${tgtlang}.filelist-systems${filt}.${tgt} ${year}.${srclang}-${tgtlang}.filelist-alignref${filt}.${tgt} | while  IFS="$(printf '\t')" read -r system ref; do
       srcfile=${ref%.*}
       nb_line_src=`wc -l ${srcfile}.${src} | cut -f1 -d' '`
@@ -55,10 +62,10 @@ for target in 'german-de' 'russian-ru'; do
       nb_line_sys=`wc -l ${system} | cut -f1 -d' '`
 
       if ([ ${nb_line_src} -eq ${nb_line_ref} ] && [ ${nb_line_ref} -eq ${nb_line_sys} ]); then
-        cat ${srcfile}.${src} >> /tmp/${srclang}-${tgtlang}.ref${filt}.${src}
-        cat ${system} >> /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}
-        cat ${system}.bleu >> /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.bleu
-        cat ${ref} >> /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt}
+        cat ${srcfile}.${src} >> /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${src}
+        cat ${system} >> /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}
+        cat ${system}.bleu >> /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.bleu
+        cat ${ref} >> /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}
       else
         echo "/!\ with the nb of line between those files: ${srcfile}.${src} (${nb_line_src} lines), ${ref} (${nb_line_ref} lines), ${system} (${nb_line_sys} lines)"
       fi
@@ -66,17 +73,20 @@ for target in 'german-de' 'russian-ru'; do
   done
 
   echo "tokenization..."
-  perl ~/workspace/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -l ${tgt} < /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt} > /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok
-  perl ~/workspace/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -l ${src} < /tmp/${srclang}-${tgtlang}.ref${filt}.${src} > /tmp/${srclang}-${tgtlang}.ref${filt}.${src}.tok
-  perl ~/workspace/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -l ${tgt} < /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt} > /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt}.tok
+  perl ~/workspace/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -threads 4 -l ${tgt} < /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt} > /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok
+  perl ~/workspace/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -threads 4 -l ${src} < /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${src} > /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${src}.tok
+  perl ~/workspace/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl -no-escape -threads 4 -l ${tgt} < /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt} > /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}.tok
 
   echo "applying seg-id script..."
-  perl ~/workspace/scripts/seg-id.pl /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok
-  perl ~/workspace/scripts/seg-id.pl /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt}.tok
+  perl ~/workspace/scripts/seg-id.pl /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok
+  perl ~/workspace/scripts/seg-id.pl /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}.tok
 
   echo "computing TER w/ tercom..."
-  java -jar /home/fblain/workspace/tools/tercom-0.7.25/tercom.7.25.jar -r /tmp/${srclang}-${tgtlang}.ref${filt}.${tgt}.tok.seg -h /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.seg -o ter -n /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.raw
-  tail -n+3 /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.raw.ter | cut -f4 -d' ' | awk 'BEGIN{OFS=FS="$"}{$1=sprintf("%0.4f",$1)}1' > /tmp/${srclang}-${tgtlang}.systems${filt}.${tgt}.tok.ter
+  java -jar /home/fblain/workspace/tools/tercom-0.7.25/tercom.7.25.jar -r /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}.tok.seg -h /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.seg -o ter -n /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.raw
+  tail -n+3 /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.raw.ter | cut -f4 -d' ' | awk 'BEGIN{OFS=FS="$"}{$1=sprintf("%0.4f",$1)}1' > /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.ter
+
+  echo "computing ChrF..."
+  python /home/fblain/workspace/tools/chrF/chrF++.py -s -R /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.ref${filt}.${tgt}.tok -H /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok > /tmp/${srclang}-${tgtlang}_${startingyear}-${endingyear}.systems${filt}.${tgt}.tok.chrf
 
 done
 
